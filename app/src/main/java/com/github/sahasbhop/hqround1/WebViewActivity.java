601f5d1d610e66664317ef8aa2ee9c031e203318
Sahasbhop Suvadhanabhakdi <sahasbhop@gmail.com>
2015-08-08 19:07:20 +0700

    Show loading progress

--- app/src/main/java/com/github/sahasbhop/hqround1/WebViewActivity.java
+++ app/src/main/java/com/github/sahasbhop/hqround1/WebViewActivity.java
@@ -1,13 +1,21 @@
 package com.github.sahasbhop.hqround1;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.github.sahasbhop.flog.FLog;
 
@@ -16,6 +24,9 @@ import butterknife.ButterKnife;
 
 public class WebViewActivity extends AppCompatActivity {
 
+    @Bind(R.id.layout_progress) View layoutProgress;
+    @Bind(R.id.progress_bar) ProgressBar progressBar;
+    @Bind(R.id.text_progress) TextView textProgress;
     @Bind(R.id.toolbar) Toolbar toolbar;
     @Bind(R.id.webview) WebView webView;
 
@@ -44,8 +55,49 @@ public class WebViewActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        progressBar.getProgressDrawable().setColorFilter(
+                getResources().getColor(R.color.accent), PorterDuff.Mode.SRC_IN);
+
+        configWebView();
+
         FLog.d("Load URL: %s", url);
         webView.loadUrl(url);
+    }
+
+    private void configWebView() {
+        webView.clearCache(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int progress) {
+                String text = String.format("%s %d%%", getString(R.string.loading_content), progress);
+                textProgress.setText(text);
+                progressBar.setProgress(progress);
+                layoutProgress.setVisibility(View.VISIBLE);
+
+                if (progress >= 100) {
+                    layoutProgress.setVisibility(View.GONE);
+                }
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                if (isFinishing()) return;
+                new AlertDialog.Builder(WebViewActivity.this)
+                        .setMessage(description)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                supportFinishAfterTransition();
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 
     @Override
